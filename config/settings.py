8a8aefbd831d55bc89ab7d21d7d04c11bc83aa7e
blackxavier <ogbuchiarinze01@gmail>
2022-05-16 12:17:52 +0100

    Remove a hardcoded django secret key

--- config/settings.py
+++ config/settings.py
@@ -1,15 +1,21 @@
 import django_on_heroku
 from pathlib import Path
+import environ
+import os
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4q_)c=azf8x(*13limu_=*427_y#_u)ic1nefwxb&mzdl&vwqk"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
